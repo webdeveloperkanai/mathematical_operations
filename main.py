3ebb77abb6c26504f6cb95d4421891a957f80636
webdevkanai <kanaishil501@gmail.com>
2021-01-12 21:31:46 +0530

    Mathematical Operations

---
+++ main.py
@@ -0,0 +1,12 @@
+from math import *
+
+firstNumber = int(input("Enter first number : "))  # user input first number
+secondNumber = int(input("Enter second number : "))  # user input second number
+print("Maximum number is : ", max(firstNumber, secondNumber)) # maximum value show
+print("Minimum number is : ", min(firstNumber, secondNumber)) # minimum value show
+print("Addition is : ", firstNumber + secondNumber) # addition of two value
+print("Substraction is : ", firstNumber - secondNumber) # substraction of two value
+print("Multiplication is : ", firstNumber * secondNumber) # multiplication of two value
+print("Devidation is : ", firstNumber / secondNumber) # devidation of two value
+print("Flore is : ", firstNumber // secondNumber) # floaring value
+print("Power of is : ", firstNumber ** secondNumber) # power of value
